7e38ffe42effe9e09bdafebb76e63fee741a568f
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-09-20 21:11:11 -0300

    alteracoes

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -5,6 +5,8 @@ const salvar = document.getElementById("salvar");
 const closed = document.getElementById("closed");
 const nome = document.getElementById("nome_content");
 const desc = document.getElementById("desc_content")
+const span1 = document.querySelector('.span-1')
+const span2 = document.querySelector('.span-2')
 
 function addTask() {
     button.addEventListener('click', function() {
@@ -30,10 +32,31 @@ closed.addEventListener('click', function() {
     menu.classList.remove("opened")
 })
 
+let arrayObject = []
+let usedIds = new Set(); 
+
 salvar.addEventListener('click', function(e) {
-    e.preventDefault()
-    const name = nome.value
-    const descricao = desc.value
-    console.log(name)
-    console.log(descricao)
-})
+    e.preventDefault();
+    const name = nome.value;
+    const descricao = desc.value;
+    let idAleatorio;
+    do {
+        idAleatorio = Date.now()
+    } while (usedIds.has(idAleatorio)); 
+
+    usedIds.add(idAleatorio);
+    
+    const objetoTarefa = {
+        name,
+        descricao, 
+        id: idAleatorio,
+        selected: false
+    };
+
+    arrayObject.push(objetoTarefa)
+
+    arrayObject.forEach(function(arrayObject) {
+        console.log(arrayObject)
+    })
+});
+
