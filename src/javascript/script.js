b9f4027a75fb5020132b52ce71a4c1565c1c278d
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-09-28 22:15:41 -0300

    alteracoes_3.0

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -5,38 +5,24 @@ const salvar = document.getElementById("salvar");
 const closed = document.getElementById("closed");
 const nome = document.getElementById("nome_content");
 const desc = document.getElementById("desc_content")
-const span1 = document.querySelector('.span-1')
-const span2 = document.querySelector('.span-2')
+const span1 = document.querySelector('.span-1');
+const span2 = document.querySelector('.span-2');
+const editar = document.getElementById('button-edit');
+const deletar = document.getElementById('button-delete');
+const emptystate = document.querySelector('.empty-state')
 
-function addTask() {
-    button.addEventListener('click', function() {
-        menu.classList.add("opened")
-    })
-    
-    fechar.addEventListener('click', function() {
-        menu.classList.remove("opened")
-    })
+function openForm() {
+    menu.classList.add("opened")
 }
 
-addTask();
-
-function salvarTask() {
-    salvar.addEventListener('click', function() {
-        menu.classList.remove("opened")
-    })
+function closeForm() {
+    menu.classList.remove("opened");
 }
-
-salvarTask();
-
-closed.addEventListener('click', function() {
-    menu.classList.remove("opened")
-})
 
 let arrayObject = []
 let usedIds = new Set(); 
 
-salvar.addEventListener('click', function(e) {
-    e.preventDefault();
+function salvarTask() {
     const name = nome.value;
     const descricao = desc.value;
     let idAleatorio;
@@ -55,8 +41,16 @@ salvar.addEventListener('click', function(e) {
 
     arrayObject.push(objetoTarefa)
 
-    arrayObject.forEach(function(arrayObject) {
-        console.log(arrayObject)
-    })
-});
+    arrayObject.forEach(function(item) {
+        console.log(item)
 
+        if (arrayObject.lenght === 0) {
+            emptystate.style.display = 'block'
+        } else {
+            emptystate.style.display = 'none';
+            const itemLista = document.createElement('li');
+            itemLista.textContent = item;
+            appendChild(itemLista);
+        }
+    })
+}
