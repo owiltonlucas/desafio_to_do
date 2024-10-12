87fc9d7beb1fc448097015dafc7eb43d932fa471
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-10-11 21:50:16 -0300

    novas_alteracoes

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,20 +2,16 @@ const button = document.getElementById("button");
 const menu = document.getElementById("menu");
 const fechar = document.getElementById("fechar");
 const salvar = document.getElementById("salvar");
-const closed = document.getElementById("closed");
-const nome = document.getElementById("nome_content");
-const desc = document.getElementById("desc_content")
-const span1 = document.querySelector('.span-1');
-const span2 = document.querySelector('.span-2');
-const editar = document.getElementById('button-edit');
-const deletar = document.getElementById('button-delete');
-const emptystate = document.querySelector('.empty-state')
+const nome = document.getElementById("nome-content");
+const desc = document.getElementById("desc-content");
+const emptystateContainer = document.querySelector('.empty-state-container'); 
 
 let arrayObject = [];
 let usedIds = new Set();
 
 function openForm() {
-    menu.classList.add("opened")
+    menu.classList.add("opened");
+    salvar.onclick = salvarTask;
 }
 
 function closeForm() {
